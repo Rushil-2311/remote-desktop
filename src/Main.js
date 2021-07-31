9957ebd3636f3d911812a48a0b8dfcb19c75ba74
Rushil-2311 <rushilpatellinux2gmail.com>
2021-07-31 18:26:10 +0530

    logo

--- src/Main.js
+++ src/Main.js
@@ -2,18 +2,20 @@ import React from 'react'
 import { Card,Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Main.css'
+import company from './company.jpeg'
 const Main = () => {
     return (
         <div>
           <Card>
   <Card.Header>Hey, Nice to see you</Card.Header>
   <Card.Body>
+    <img src={company} alt="" style={{width:"400px",marginBottom:"30px"}} />
     <Card.Title>This is my remote desktop project</Card.Title>
     <Card.Text>
       you can click below link to move in my project
     </Card.Text>
     <Button variant="primary">
-    <a href="https://remotedesktop.google.com">GO SOMEWERE</a>
+    <a href="https://remotedesktop.google.com">GO To RemoteDesktop</a>
     </Button>
   </Card.Body>
 </Card>
